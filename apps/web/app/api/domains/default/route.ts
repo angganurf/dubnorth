fd0a7ddf9c69f0dc1f22a4aebd43e8783dbc61c1
angganurf <angganurfaizal234@gmail.com>
2024-06-17 00:10:05 +0700

    Third

--- apps/web/app/api/domains/default/route.ts
+++ apps/web/app/api/domains/default/route.ts
@@ -11,7 +11,7 @@ export const GET = withWorkspace(async ({ workspace }) => {
       projectId: workspace.id,
     },
     select: {
-      injmonks: true,
+      injmonkssite: true,
       chatgpt: true,
       sptifi: true,
       gitnew: true,
@@ -48,7 +48,7 @@ export const PUT = withWorkspace(async ({ req, workspace }) => {
       projectId: workspace.id,
     },
     data: {
-      injmonks: defaultDomains.includes("l.injmonks.site"),
+      injmonkssite: defaultDomains.includes("injmonks.site"),
       chatgpt: defaultDomains.includes("chatg.pt"),
       sptifi: defaultDomains.includes("spti.fi"),
       gitnew: defaultDomains.includes("git.new"),
